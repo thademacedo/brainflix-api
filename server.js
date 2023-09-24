338ab477bcd2c3a1144021ee665d549b97859db2
Thais De Macedo <thademacedo@gmail.com>
2023-09-24 16:02:36 -0700

    adding post comment function

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 const port = process.env.PORT || 8080;
 
-const cors = require('cors');
-const express = require('express');
+const cors = require("cors");
+const express = require("express");
 const app = express();
-const fs = require('fs');
+const fs = require("fs");
+const uniqid = require("uniqid");
 
 app.use(express.json());
 app.use(cors());
@@ -13,14 +14,28 @@ app.use(cors());
 const videosDetailed = JSON.parse(
   fs.readFileSync(`./assets/data/video-details.json`)
 );
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.status(200).json(videosDetailed);
 });
 
-app.get('/videos/:id', (req, res) => {
+app.get("/videos/:id", (req, res) => {
   const id = req.params.id;
   const video = videosDetailed.find((el) => el.id === id);
   res.status(200).json(video);
+});
+app.post("/videos/:id", (req, res) => {
+  const newId = uniqid();
+  const newComment = Object.assign({ id: newId }, req.body);
+  const videoId = req.params.id;
+  const video = videosDetailed.find((el) => el.id === videoId);
+  video.comments.push(newComment);
+  fs.writeFile(
+    `./assets/data/video-details.json`,
+    JSON.stringify(videosDetailed),
+    (err) => {
+      res.status(201).json(video);
+    }
+  );
 });
 
 app.listen(port, () => {
